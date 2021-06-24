d7673fe12ecdc7c80d7f0d60a501ebd6e9d8d6c5
terminalparty <108016653+terminalparty@users.noreply.github.com>
2021-06-24 19:49:58 +0000

    add change

--- lib/coingecko_ruby/models/api_link.rb
+++ lib/coingecko_ruby/models/api_link.rb
@@ -2,3 +2,46 @@
 # frozen_string_literal: true
 
 module CoingeckoRuby
+  module Models
+    class ApiLink
+      API_ENDPOINT = 'https://api.coingecko.com/api/v3'
+
+      def self.list_url
+        "#{API_ENDPOINT}/coins/list"
+      end
+
+      def self.simple_url(ids, vs_currencies, include_market_cap, include_24hr_vol, include_24hr_change, include_last_updated_at)
+        "#{API_ENDPOINT}/simple/price?ids=#{ids}&vs_currencies=#{vs_currencies}&include_market_cap=#{include_market_cap}&include_24hr_vol=#{include_24hr_vol}&include_24hr_change=#{include_24hr_change}&include_last_updated_at=#{include_last_updated_at}"
+      end
+
+      def self.simple_token_price_url(id, contract_addresses, vs_currencies, include_market_cap, include_24hr_vol, include_24hr_change, include_last_updated_at)
+        "#{API_ENDPOINT}/simple/token_price/#{id}?contract_addresses=#{contract_addresses}&vs_currencies=#{vs_currencies}&include_market_cap=#{include_market_cap}&include_24hr_vol=#{include_24hr_vol}&include_24hr_change=#{include_24hr_change}&include_last_updated_at=#{include_last_updated_at}"
+      end
+
+      def self.coin_url(id, localization, tickers, market_data, community_data, developer_data, sparkline)
+        "#{API_ENDPOINT}/coins/#{id}?localization=#{localization}&tickers=#{tickers}&market_data=#{market_data}&community_data=#{community_data}&developer_data=#{developer_data}&sparkline=#{sparkline}"
+      end
+
+      def self.coin_markets_url(vs_currency, ids, order, per_page, page, sparkline, price_change_percentage)
+        "#{API_ENDPOINT}/coins/markets/?vs_currency=#{vs_currency}&ids=#{ids}&order=#{order}&per_page=#{per_page}&page=#{page}&sparkline=#{sparkline}&price_change_percentage=#{price_change_percentage}"
+      end
+
+      def self.coin_tickers_url(id, exchange_ids, include_exchange_logo, page, order)
+        "#{API_ENDPOINT}/coins/#{id}/tickers?exchange_ids=#{exchange_ids}&include_exchange_logo=#{include_exchange_logo}&page=#{page}&order=#{order}"
+      end
+
+      def self.coin_history_url(id, date, localization)
+        "#{API_ENDPOINT}/coins/#{id}/history?date=#{date}&localization=#{localization}"
+      end
+
+      def self.coin_market_chart_url(id, vs_currency, days)
+        "#{API_ENDPOINT}/coins/#{id}/market_chart?vs_currency=#{vs_currency}&days=#{days}"
+      end
+
+      def self.coin_market_chart_in_range_url(id, vs_currency, from, to)
+        "#{API_ENDPOINT}/coins/#{id}/market_chart/range?vs_currency=#{vs_currency}&from=#{from}}&to=#{to}"
+      end
+
+      def self.coin_status_updates_url(id)
+        "#{API_ENDPOINT}/coins/#{id}/status_updates"
+      end
