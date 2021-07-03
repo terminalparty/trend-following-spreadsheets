caa7244dc0d90755a15955f1afafde0bfabaf1bc
terminalparty <108016653+terminalparty@users.noreply.github.com>
2021-07-03 17:29:58 +0000

    css file

--- lib/coingecko_ruby/models/coin.rb
+++ lib/coingecko_ruby/models/coin.rb
@@ -5,4 +5,5 @@ module CoingeckoRuby
     class Coin
       attr_accessor :id, :symbol, :name,
                     :block_time_in_minutes, :categories, :localization,
-           
+                    :description, :links, :image,
+                    :cou
