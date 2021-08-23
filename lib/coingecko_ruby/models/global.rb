447893376f3c03a4d83359cf50e3f2777dadc5fe
terminalparty <108016653+terminalparty@users.noreply.github.com>
2021-08-23 08:31:06 +0000

    new texts

--- lib/coingecko_ruby/models/global.rb
+++ lib/coingecko_ruby/models/global.rb
@@ -4,4 +4,4 @@ module CoingeckoRuby
   module Models
     class Global
       attr_accessor :active_cryptocurrencies, :upcoming_icos, :ongoing_icos, :ended_icos, :markets,
-                    :total
+                    :total_market_cap, :total_volume, :market_cap_percentag
