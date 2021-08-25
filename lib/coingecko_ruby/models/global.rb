0f4cdc556a573febd778cf5a918c2c9efe0c79be
terminalparty <108016653+terminalparty@users.noreply.github.com>
2021-08-25 08:11:26 +0000

    power: Add check_wakeup_reason() to verify wakeup source irq

--- lib/coingecko_ruby/models/global.rb
+++ lib/coingecko_ruby/models/global.rb
@@ -4,4 +4,5 @@ module CoingeckoRuby
   module Models
     class Global
       attr_accessor :active_cryptocurrencies, :upcoming_icos, :ongoing_icos, :ended_icos, :markets,
-                    :total_market_cap, :total_volume, :market_cap_percentag
+                    :total_market_cap, :total_volume, :market_cap_percentage,
+                    :market_cap_change_percentage_24
