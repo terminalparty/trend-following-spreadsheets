ba21c94d1d9c26002ac0c5810c58836c7cad8e80
terminalparty <108016653+terminalparty@users.noreply.github.com>
2021-08-28 15:21:44 +0000

    Regenerated gemspec for version 0.0.8

--- lib/coingecko_ruby/models/global.rb
+++ lib/coingecko_ruby/models/global.rb
@@ -5,4 +5,7 @@ module CoingeckoRuby
     class Global
       attr_accessor :active_cryptocurrencies, :upcoming_icos, :ongoing_icos, :ended_icos, :markets,
                     :total_market_cap, :total_volume, :market_cap_percentage,
-                    :market_cap_change_percentage_24
+                    :market_cap_change_percentage_24h_usd, :updated_at
+    end
+  end
+end
