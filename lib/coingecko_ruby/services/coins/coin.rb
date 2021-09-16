a16611021d575a17aaa0b92b4739c4e2847cfda2
terminalparty <108016653+terminalparty@users.noreply.github.com>
2021-09-16 08:12:20 +0000

    Create blank.txt

--- lib/coingecko_ruby/services/coins/coin.rb
+++ lib/coingecko_ruby/services/coins/coin.rb
@@ -3,4 +3,5 @@
 module CoingeckoRuby::Services::Coins
   class Coin < CoingeckoRuby::Services
     def fetch(id, localization, tickers,
-              
+              market_data, community_data, developer_data,
+  
