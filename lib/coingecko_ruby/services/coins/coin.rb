bf36bb49bb3689ef1f68fa7b4cf74a8c43563366
terminalparty <108016653+terminalparty@users.noreply.github.com>
2021-10-11 14:50:42 +0000

    id

--- lib/coingecko_ruby/services/coins/coin.rb
+++ lib/coingecko_ruby/services/coins/coin.rb
@@ -27,4 +27,4 @@ module CoingeckoRuby::Services::Coins
       coin.country_origin = output['country_origin']
       coin.genesis_date = output['genesis_date']
       coin.market_cap_rank = output['market_cap_rank']
-  
+      coin.coingecko_rank = output['coing
