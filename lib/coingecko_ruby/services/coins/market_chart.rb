0503d0943f617c98c89f112eadf1bcd33871ad3d
terminalparty <108016653+terminalparty@users.noreply.github.com>
2021-11-30 18:34:33 +0000

    Travis uses bin/phpunit for right PHPUnit version

--- lib/coingecko_ruby/services/coins/market_chart.rb
+++ lib/coingecko_ruby/services/coins/market_chart.rb
@@ -3,4 +3,5 @@
 module CoingeckoRuby::Services::Coins
   class MarketChart < CoingeckoRuby::Services
     def fetch(id, vs_currency, days)
-      output = super(CoingeckoRuby::Models::ApiLink.
+      output = super(CoingeckoRuby::Models::ApiLink.coin_market_chart_url(
+        id, vs_currency, days
