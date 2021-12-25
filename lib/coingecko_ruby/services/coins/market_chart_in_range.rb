31331603e9b8c40b1e8f3379e8f1fd262e0545f1
terminalparty <108016653+terminalparty@users.noreply.github.com>
2021-12-25 07:21:01 +0000

    fixed spelling mistakes

--- lib/coingecko_ruby/services/coins/market_chart_in_range.rb
+++ lib/coingecko_ruby/services/coins/market_chart_in_range.rb
@@ -3,4 +3,5 @@
 module CoingeckoRuby::Services::Coins
   class MarketChartInRange < CoingeckoRuby::Services
     def fetch(id, vs_currency, from, to)
-      output = super(CoingeckoRuby::Models::ApiLink.coin_ma
+      output = super(CoingeckoRuby::Models::ApiLink.coin_market_chart_in_range_url(
+        id, vs_curr
