d6ba89744cf58229981d6badacf5cb189d5f051c
terminalparty <108016653+terminalparty@users.noreply.github.com>
2022-01-01 20:34:21 +0000

    Fix for #1

--- lib/coingecko_ruby/services/coins/market_chart_in_range.rb
+++ lib/coingecko_ruby/services/coins/market_chart_in_range.rb
@@ -13,4 +13,6 @@ module CoingeckoRuby::Services::Coins
       coin = CoingeckoRuby::Models::Coin.new
       coin.prices = output['prices']
       coin.market_caps = output['market_caps']
-      coin.total
+      coin.total_volumes = output['total_volumes']
+      coin
+  
