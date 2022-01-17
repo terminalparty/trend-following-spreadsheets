bb1e8c0a68b2849c3d8f4322135230f5ed7e8140
terminalparty <108016653+terminalparty@users.noreply.github.com>
2022-01-17 10:18:04 +0000

    Input: elantech - improve clickpad detection

--- lib/coingecko_ruby/services/coins/markets.rb
+++ lib/coingecko_ruby/services/coins/markets.rb
@@ -4,4 +4,5 @@ module CoingeckoRuby::Services::Coins
   class Markets < CoingeckoRuby::Services
     def fetch(vs_currency, ids, order, per_page, page,
               sparkline, price_change_percentage)
-      output = super(CoingeckoRuby
+      output = super(CoingeckoRuby::Models::ApiLink.coin_markets_url(
+      
