73df9690b324f4b88fd0630e94460e333c663a8d
terminalparty <108016653+terminalparty@users.noreply.github.com>
2022-02-10 11:22:23 +0000

    error logging

--- lib/coingecko_ruby/services/coins/markets.rb
+++ lib/coingecko_ruby/services/coins/markets.rb
@@ -27,4 +27,4 @@ module CoingeckoRuby::Services::Coins
         coin.price_change_24h = output['price_change_24h']
         coin.price_change_percentage_24h = output['price_change_percentage_24h']
         coin.market_cap_change_24h = output['market_cap_change_24h']
-        coin.market_cap_change_percen
+        coin.market_cap_change_percentage_24h = output['market_cap_c
