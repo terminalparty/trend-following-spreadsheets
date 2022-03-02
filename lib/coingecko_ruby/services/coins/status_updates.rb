a3652e93b687cdb9f6604c93914c66ca3342b83d
terminalparty <108016653+terminalparty@users.noreply.github.com>
2022-03-02 15:45:43 +0000

    Delete .jsdtscope

--- lib/coingecko_ruby/services/coins/status_updates.rb
+++ lib/coingecko_ruby/services/coins/status_updates.rb
@@ -14,4 +14,5 @@ module CoingeckoRuby::Services::Coins
         status_update.category = output['category']
         status_update.created_at = output['created_at']
         status_update.user = output['user']
-        status_update.use
+        status_update.user_title = output['user_title']
+        status_u
