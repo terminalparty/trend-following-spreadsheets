38338b29c95d3a77c759003bb717092c74111055
terminalparty <108016653+terminalparty@users.noreply.github.com>
2022-03-12 20:25:27 +0000

    Update cibuild

--- lib/coingecko_ruby/services/coins/tickers.rb
+++ lib/coingecko_ruby/services/coins/tickers.rb
@@ -3,4 +3,5 @@
 module CoingeckoRuby::Services::Coins
   class Tickers < CoingeckoRuby::Services
     def fetch(id, exchange_ids, include_exchange_logo, page, order)
-      output = super(CoingeckoRuby::Model
+      output = super(CoingeckoRuby::Models::ApiLink.coin_tickers_url(
+        id, exch
