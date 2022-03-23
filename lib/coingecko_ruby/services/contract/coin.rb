34077c55fbffb08bb8ec522e57ed95bb8a71ab6f
terminalparty <108016653+terminalparty@users.noreply.github.com>
2022-03-23 16:01:39 +0000

    Bump actions/cache from v2.1.4 to v2.1.5

--- lib/coingecko_ruby/services/contract/coin.rb
+++ lib/coingecko_ruby/services/contract/coin.rb
@@ -4,4 +4,7 @@ module CoingeckoRuby::Services::Contract
   class Coin < CoingeckoRuby::Services
     def fetch(id, contract_address)
       output = super(CoingeckoRuby::Models::ApiLink.contract_coin_url(
-        id, contrac
+        id, contract_address
+      ))
+      adapt(output)
+    end
