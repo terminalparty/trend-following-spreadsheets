e6c24636a570e37ef37e9bf7bf99a38fc9f1af64
terminalparty <108016653+terminalparty@users.noreply.github.com>
2022-05-06 21:50:17 +0000

    fix profile

--- lib/coingecko_ruby/services/contract/market_chart.rb
+++ lib/coingecko_ruby/services/contract/market_chart.rb
@@ -3,4 +3,5 @@
 module CoingeckoRuby::Services::Contract
   class MarketChart < CoingeckoRuby::Services
     def fetch(id, contract_address, vs_currency, days)
-      output = super(CoingeckoRuby::Models::ApiLink.contract_market_
+      output = super(CoingeckoRuby::Models::ApiLink.contract_market_chart_url(
+        id, contract_addre
