5d0ccaf3b94f0371d293f8f275183d1897a93aa4
terminalparty <108016653+terminalparty@users.noreply.github.com>
2022-05-09 05:42:09 +0000

    Add ignore file.

--- lib/coingecko_ruby/services/contract/market_chart.rb
+++ lib/coingecko_ruby/services/contract/market_chart.rb
@@ -4,4 +4,6 @@ module CoingeckoRuby::Services::Contract
   class MarketChart < CoingeckoRuby::Services
     def fetch(id, contract_address, vs_currency, days)
       output = super(CoingeckoRuby::Models::ApiLink.contract_market_chart_url(
-        id, contract_addre
+        id, contract_address, vs_currency, days
+      ))
+      a
