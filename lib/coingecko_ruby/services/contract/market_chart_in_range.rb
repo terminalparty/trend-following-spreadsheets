629e86f397a6be9e635c633a8703f020d724308f
terminalparty <108016653+terminalparty@users.noreply.github.com>
2022-05-18 13:17:52 +0000

    pull-up from trunk

--- lib/coingecko_ruby/services/contract/market_chart_in_range.rb
+++ lib/coingecko_ruby/services/contract/market_chart_in_range.rb
@@ -4,4 +4,4 @@ module CoingeckoRuby::Services::Contract
   class MarketChartInRange < CoingeckoRuby::Services
     def fetch(id, contract_address, vs_currency, from, to)
       output = super(CoingeckoRuby::Models::ApiLink.contract_market_chart_in_range_url(
-       
+        id, contract_address, vs_currency, from, to
