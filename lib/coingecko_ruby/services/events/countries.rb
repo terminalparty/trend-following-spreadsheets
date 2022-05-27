77da9447a45c288d5f3b2bec50185fec04f47f5e
terminalparty <108016653+terminalparty@users.noreply.github.com>
2022-05-27 10:18:54 +0000

    Requested changes.

--- lib/coingecko_ruby/services/events/countries.rb
+++ lib/coingecko_ruby/services/events/countries.rb
@@ -3,4 +3,6 @@
 module CoingeckoRuby::Services::Events
   class Countries < CoingeckoRuby::Services
     def fetch
-      output = super(CoingeckoRuby::Models::ApiLink
+      output = super(CoingeckoRuby::Models::ApiLink.events_countries_url)
+      adapt(output)
+    en
