2d239c958fc2f94bf4a4f873b14cb4cbf8af8f36
terminalparty <108016653+terminalparty@users.noreply.github.com>
2022-06-27 09:47:30 +0000

    Add missing requires

--- lib/coingecko_ruby/services/exchanges/exchange.rb
+++ lib/coingecko_ruby/services/exchanges/exchange.rb
@@ -3,4 +3,8 @@
 module CoingeckoRuby::Services::Exchanges
   class Exchange < CoingeckoRuby::Services
     def fetch(id)
-      output = super(CoingeckoRuby::Models::ApiLink.exc
+      output = super(CoingeckoRuby::Models::ApiLink.exchange_url(id))
+      adapt(output)
+    end
+
+    
