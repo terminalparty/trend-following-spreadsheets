44a340320e6d92209e60238b6951f2bac17d3c0f
terminalparty <108016653+terminalparty@users.noreply.github.com>
2022-07-22 20:54:25 +0000

    Bumped version to 1.0.1.

--- lib/coingecko_ruby/services/exchanges/exchanges.rb
+++ lib/coingecko_ruby/services/exchanges/exchanges.rb
@@ -21,3 +21,4 @@ module CoingeckoRuby::Services::Exchanges
         exchange.image = output['image']
         exchange.has_trading_incentive = output['has_trading_incentive']
         exchange.trade_volume_24h_btc = output['trade_volume_24h_btc']
+        exchange.trade_volume_24h_btc_norm
