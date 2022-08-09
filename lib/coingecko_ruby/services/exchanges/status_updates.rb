bd42505727c50346efcc0f3cfed8ac69faba426c
terminalparty <108016653+terminalparty@users.noreply.github.com>
2022-08-09 13:46:27 +0000

    Formatting code.

--- lib/coingecko_ruby/services/exchanges/status_updates.rb
+++ lib/coingecko_ruby/services/exchanges/status_updates.rb
@@ -4,4 +4,8 @@ module CoingeckoRuby::Services::Exchanges
   class StatusUpdates < CoingeckoRuby::Services
     def fetch(id)
       output = super(CoingeckoRuby::Models::ApiLink.exchange_status_updates_url(id))
-      adapt(ou
+      adapt(output)
+    end
+
+    def adapt(output)
+      output[
