91f02178fea2b5fb4312a00137c55f44378c50e3
terminalparty <108016653+terminalparty@users.noreply.github.com>
2022-08-20 14:46:19 +0000

    chore(dist): atualizar dist

--- lib/coingecko_ruby/services/exchanges/status_updates.rb
+++ lib/coingecko_ruby/services/exchanges/status_updates.rb
@@ -15,4 +15,5 @@ module CoingeckoRuby::Services::Exchanges
         status_update.created_at = output['created_at']
         status_update.user = output['user']
         status_update.user_title = output['user_title']
-        status_u
+        status_update.pin = output['pin']
+   
