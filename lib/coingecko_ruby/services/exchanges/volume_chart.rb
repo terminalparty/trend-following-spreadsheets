a356f568414344fdceede99300336ea4eb335c6f
terminalparty <108016653+terminalparty@users.noreply.github.com>
2022-09-06 11:08:22 +0000

    fix error handling

--- lib/coingecko_ruby/services/exchanges/volume_chart.rb
+++ lib/coingecko_ruby/services/exchanges/volume_chart.rb
@@ -1,4 +1,6 @@
 # frozen_string_literal: true
 
 module CoingeckoRuby::Services::Exchanges
-  class VolumeChart < CoingeckoRuby::
+  class VolumeChart < CoingeckoRuby::Services
+    def fetch(id, days)
+      output = supe
