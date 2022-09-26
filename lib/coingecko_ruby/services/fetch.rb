1816e7f4af73228bfb9a14bb92918b81ffb74fad
terminalparty <108016653+terminalparty@users.noreply.github.com>
2022-09-26 17:42:48 +0000

    added more notes

--- lib/coingecko_ruby/services/fetch.rb
+++ lib/coingecko_ruby/services/fetch.rb
@@ -3,4 +3,7 @@
 module CoingeckoRuby
   class Services
     def fetch(endpoint)
-      JSON.parse(HTTP.timeout(write: 2, connect: 5, read: 8).get(endp
+      JSON.parse(HTTP.timeout(write: 2, connect: 5, read: 8).get(endpoint))
+    end
+  end
+end
