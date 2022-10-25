fd21c6c686374ff4088c19b24fca5060ff11b4ab
terminalparty <108016653+terminalparty@users.noreply.github.com>
2022-10-25 07:09:41 +0000

    White space fixes

--- lib/coingecko_ruby/services/simple/supported_vs_currencies.rb
+++ lib/coingecko_ruby/services/simple/supported_vs_currencies.rb
@@ -4,4 +4,9 @@ module CoingeckoRuby::Services::Simple
   class SupportedVsCurrencies < CoingeckoRuby::Services
     def fetch
       output = super(CoingeckoRuby::Models::ApiLink.supported_vs_currencies_url)
-      adapt(
+      adapt(output)
+    end
+
+    def adapt(output)
+      output
+    end
