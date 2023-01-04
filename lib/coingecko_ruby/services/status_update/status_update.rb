f9b04d6b919cf7098cf7ff959d45ee634622cfd7
terminalparty <108016653+terminalparty@users.noreply.github.com>
2023-01-04 18:08:15 +0000

    Code style changes.

--- lib/coingecko_ruby/services/status_update/status_update.rb
+++ lib/coingecko_ruby/services/status_update/status_update.rb
@@ -15,4 +15,5 @@ module CoingeckoRuby::Services::StatusUpdate
         status_update.category = output['category']
         status_update.created_at = output['created_at']
         status_update.user = output['user']
-        status_update.user_title 
+        status_update.user_title = output['user_title']
+        status_update.pin
