f8f8ea92f4f8a4306c0b9c6e74cc62facdd6ce43
terminalparty <108016653+terminalparty@users.noreply.github.com>
2023-04-01 15:27:07 +0000

    more tests

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,4 +5,5 @@ RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
   config.example_status_persistence_file_path = ".rspec_status"
 
-  # Disable RSpec exposing meth
+  # Disable RSpec exposing methods globally on `Module` and `main`
+  config.
