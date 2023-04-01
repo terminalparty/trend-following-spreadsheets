a3261fd01f8f715ae17f2077c8ff3269cd512160
terminalparty <108016653+terminalparty@users.noreply.github.com>
2023-04-01 11:30:55 +0000

    .m

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,4 +3,6 @@ require "coingecko/ruby"
 
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
-  config.example_status_persistence_file_path = 
+  config.example_status_persistence_file_path = ".rspec_status"
+
+  # Disable RSpec exposing meth
